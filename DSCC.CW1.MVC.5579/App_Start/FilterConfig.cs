0a515aec1219c7658ff003304dc69a003316b317
Pardon <shahnozkarimova1997@gmail.com>
2020-03-02 06:59:51 +0500

    First commit

---
+++ DSCC.CW1.MVC.5579/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DSCC.CW1.MVC._5579
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DSCC.CW1.MVC.5579/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DSCC.CW1.MVC._5579.MvcApplication" Language="C#" %>
